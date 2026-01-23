65b2ba5f06ece55e18a5938bc39858ee17f26595
kuratajr <n19dcat085@student.ptithcm.edu.vn>
2026-01-23 14:58:01 +0700

    FIX: Fix terminal.

--- src/dashboard.ts
+++ src/dashboard.ts
@@ -3,8 +3,8 @@ import styles from './dashboard/styles.css';
 // @ts-ignore
 import scripts from './dashboard/scripts.js';
 // @ts-ignore
-import htmlTemplate from './dashboard/index.html';
+import htmlTemplate from './dashboard/dashboard.html';
 
 export const DASHBOARD_HTML = htmlTemplate
-    .replace(/\{\{STYLES\}\}/g, styles)
-    .replace(/\{\{SCRIPTS\}\}/g, scripts);
+    .replace('__STYLES_CONTENT__', styles)
+    .replace('__SCRIPTS_CONTENT__', scripts);
